9a87b4d08fd5b13d7a9420a30329ef503f5e3f0f
Zack Anderson <zack.anderson2011@gmail.com>
2021-07-24 14:57:01 -0500

    employeee.test.js complete

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,18 +2,23 @@ const Employee = require("../lib/Employee");
 
 describe("Employee class", () => {
     it("can be instantiated", () => {
-        const employee = new Employee();
-        expect(typeof employee).toBe("object");
-        expect(employee instanceof Employee).toBe(true);
-    });
+                const employee = new Employee();
+                expect(typeof employee).toBe("object");
+                expect(employee instanceof Employee).toBe(true);
+            });
 
-    it("should have a name property that can be set on instanstiation", () => {
-        const employee = new Employee("Dave");
-        expect(employee.name).toBe("Dave");
-    });
+            it('should have a name property that can be set on instanstiation', () => {
+                const employee = new Employee('Zack');
+                expect(employee.name).toBe('Zack');
+            })
 
-    it("should have a id property that can be set on instanstiation", () => {
-        const employee = new Employee("Dave", 1);
-        expect(employee.id).toBe(1);
-    });
-});
+            it('should have an id property that can be set on instanstiation', () => {
+                const employee = new Employee('Zack', 1);
+                expect(employee.id).toBe(1);
+            })
+
+            it('should have an email property that can be set on instanstiation', () => {
+                const email = "zack.anderson2011@gmail.com"
+                const employee = new Employee('Zack', 1, email);
+                expect(employee.email).toBe(email);
+            })
