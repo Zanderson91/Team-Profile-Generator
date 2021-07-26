afe69af719ecb195aaa7fa300562ddec6de5fa08
Zack Anderson <zack.anderson2011@gmail.com>
2021-07-26 12:53:54 -0500

    cleaned up variables to be global vairables

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,9 @@
 const Engineer = require("../lib/Engineer");
 
 describe("Engineer class", () => {
+    const email = "zack.anderson2011@gmail.com";
+    const github = 'Zanderson91';
+
     it("can be instantiated", () => {
         const engineer = new Engineer();
         expect(typeof engineer).toBe("object");
@@ -18,14 +21,18 @@ describe("Engineer class", () => {
     })
 
     it('should have an email property that can be set on instanstiation', () => {
-        const email = "zack.anderson2011@gmail.com";
         const engineer = new Engineer('Zack', 1, email);
         expect(engineer.email).toBe(email);
     })
 
+    /*need to globally assign variables describe("Engineer class", () => {
+    define all of your variables and create your Engineer here, then run all of the tests on that one engineer
+    it("can be instantiated", () => {
+        */
+
     it('should have a Github property that can be set on instanstiation', () => {
-        const github = 'Zanderson91';
-        const engineer = new Engineer('Zack', 1 , github);
+        const engineer = new Engineer('Zack', 1, email, github);
+        console.log(engineer)
         expect(engineer.github).toBe(github);
     })
 
