ea73f828225182e3da4c49fbe48fbbaf9bca2922
Zack Anderson <zack.anderson2011@gmail.com>
2021-07-26 13:22:13 -0500

    working through index.js..setting variables and about to write prompts.

---
+++ index.js
@@ -0,0 +1,7 @@
+const generateCards = require("./src/generateCards");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const teamArray = [];
