91e35528f7532e49835497843690948b9c150624
Zack Anderson <zack.anderson2011@gmail.com>
2021-07-26 13:33:34 -0500

    array of prompts provided for manager new function created to return the listed data

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const teamArray = [];
+
+const addManager = () => {
+    return inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Hello, Manager! What is your name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is your ID #?",
+            name: "id",
+        },
+        {
+            type: "message",
+            message: "What is your email address?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What is your office #?",
+            name: "email",
+        },
+    ])
+    .then((managerResponse)) => {
+        const {name, id, email, officeNumber} = managerResponse;
+        const manager = new Manager(name, id, email, officeNumber);
+        
+    }
+}
