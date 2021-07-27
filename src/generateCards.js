2b6af778abf8bfd5c67c177c5d45fa699b337312
Zack Anderson <zack.anderson2011@gmail.com>
2021-07-27 08:40:16 -0500

    WIP...created for each loop to determine how each card should be combined and returned

--- src/generateCards.js
+++ src/generateCards.js
@@ -31,3 +31,106 @@ function generateIntern(intern) {
     </div>`
 };
 
+(generateCards = (data) => {
+  pageArray = [];
+
+  for (var i = 0; i < data.length; i++) {
+    const employee = data[i];
+    const role = employee.getRole();
+    if (role === "Manager") {
+      const managerCard = generateManager(employee);
+      pageArray.push(managerCard);
+    }
+    if (role === "Engineer") {
+      const engineerCard = generateEngineer(employee);
+      pageArray.push(engineerCard);
+    }
+    if (role === "Intern") {
+      const internCard = generateIntern(employee);
+      pageArray.push(internCard);
+    }
+  }
+  const employeeCards = pageArray.join(" ");
+  const generateTeam = generateTeamPage(employeeCards);
+  return generateTeam;
+})
+
+//function that will generate the main html template
+function generateTeamPage(employeeCards) {
+  return `<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+            crossorigin="anonymous" />
+        <link rel="stylesheet" href="./style.css" />
+        <title>Team Profile Generator</title>
+    </head>
+
+    <body>
+
+        <header>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid d-flex justify-content-center">
+                    <span class="navbar-brand m-5 h1">My Team</span>
+                </div>
+            </nav>
+        </header>
+        <br>
+        <br>
+
+        <main>
+${employeeCards}
+            <div class="row justify-content-center">
+                <div class="col-sm-2">
+                    <div class="card text-white bg-dark" style="width: 20
+                            rem">
+                        <div class="card-header mt-3 mb-3">Manger: </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item m-2"> ID: </li>
+                            <li class="list-group-item m-2">Email: <a href="mailto:">
+                                    </a></li>
+                            <li class="list-group-item m-2">Office Phone:
+                                </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="card text-white bg-dark" style="width: 20
+                            rem">
+                        <div class="card-header mt-3 mb-3">Engineer:</div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item m-2"> ID: </li>
+                            <li class="list-group-item m-2">Email: <a href="mailto:">
+                                    </a></li>
+                            <li class="list-group-item m-2">Github:
+                                </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="card text-white bg-dark" style="width: 20
+                            rem">
+                        <div class="card-header mt-3 mb-3">Intern: </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item m-2"> ID: </li>
+                            <li class="list-group-item m-2">Email: <a href="mailto:"></a></li>
+                            <li class="list-group-item m-2">School:
+                                </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script src="../index.js"></script>
+    </body>
+
+</html>
+`;
+}
+
+module.exports = generateCards;
